35394b3369948925083d73929f8c900c8f1c7ed6
xamgore <xamgore@ya.ru>
2015-01-14 00:34:26 +0300

    Particle system contains color info

--- src/com/xamgore/particles/game/ParticleSystem.java
+++ src/com/xamgore/particles/game/ParticleSystem.java
@@ -14,9 +14,36 @@ public class ParticleSystem {
     private final static int MAX_PARTICLES_HARDMODE = 380;
     private final static int MAX_PARTICLES = 280;
 
+    private int colorThemeNum;
+    private final static int[][] COLOURS = {
+            {0xff69D2E7, 0xffA7DBD8, 0xffE0E4CC,
+                    0xffF38630, 0xffFA6900, 0xffFF4E50, 0xffF9D423},
+            {0xfffbd887, 0xffc6d96d, 0xff2cd7b7, 0xff0f747c},
+            {0xfffef5ba, 0xffd99290, 0xff7d9f8f, 0xff70526e, 0xff40385d},
+            {0xfff9af56, 0xffee655b, 0xffc93766, 0xff532a28, 0xffffffff},
+            {0xffb4cb85, 0xfff16b50, 0xffea2540, 0xff019690},
+            {0xfff1ab17, 0xfffe7e11, 0xff99c91b, 0xff69b6dc},
+            {0xffb2f1ff, 0xffc2a2ad, 0xff989b90, 0xff645365},
+            {0xfffef4b6, 0xffffd6a2, 0xffafd0fd, 0xff5785e3},
+    };
 
     public ParticleSystem() {
         particles = new LinkedList<>();
+    }
+
+
+    public void pickRandomColor() {
+        Particle.colours = COLOURS[colorThemeNum = rnd.nextInt(COLOURS.length)];
+    }
+
+    public void pickNextColorTheme() {
+        colorThemeNum = (colorThemeNum + 1) % COLOURS.length;
+        Particle.colours = COLOURS[colorThemeNum];
+    }
+
+    public void pickPrevColorTheme() {
+        colorThemeNum = (COLOURS.length + colorThemeNum - 1) % COLOURS.length;
+        Particle.colours = COLOURS[colorThemeNum];
     }
 
 
